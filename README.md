f7ac173e5c92d1a8b894df643425db0a5679f739
molgeek <74042620+morgueye4@users.noreply.github.com>
2025-01-12 20:02:51 +0000

    Relai DAO App foundations

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# relai-apps
-Relai DAO Apps 
+# Relai DAO Apps 
+
+<img src="./assets/logo.png" alt="the first starsheep" width="300">
+
+Reliable AI Platform
---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.tsx"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        parser: "@typescript-eslint/parser",
+      },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
+    },
+  },
+  prettier,
+];
